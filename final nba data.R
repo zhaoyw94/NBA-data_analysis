9aca216a70d24042e7c26655da3515be9a6e86ba
Jiali <34532375+zhaoyw94@users.noreply.github.com>
2020-06-29 12:03:13 -0700

    Add files via upload

---
+++ final nba data.R
@@ -0,0 +1,26 @@
+
+#salary
+
+myplot(Salary)
+myplot(Salary/Games)
+myplot(Salary/FieldGoals)
+
+# In-Game Metrics
+myplot(MinutesPlayed)
+myplot(Points)
+
+# In-Game Metrics Normalized
+myplot(FieldGoals/Games)
+myplot(FieldGoals/FieldGoalAttempts)
+myplot(FieldGoalAttempts/Games)
+myplot(Points/Games)
+
+#Interesting Observation
+myplot(MinutesPlayed/Games)
+myplot(Games)
+
+#time is valueable
+myplot(FieldGoals/MinutesPlayed)
+
+#player style
+myplot(Points/FieldGoals)
